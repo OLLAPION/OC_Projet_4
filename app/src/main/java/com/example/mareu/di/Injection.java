0c3907680cf4bd062f261878a828e3dd302d2f89
OLLAPION <ollapion@gmail.com>
2023-01-27 20:20:50 +0100

    Injection qui retourne le repository avec un new ApiService

--- app/src/main/java/com/example/mareu/di/Injection.java
+++ app/src/main/java/com/example/mareu/di/Injection.java
@@ -1,2 +1,12 @@
-package com.example.mareu.di;public class Injection {
+package com.example.mareu.di;
+
+import com.example.mareu.repository.ReunionRepository;
+import com.example.mareu.service.DummyReunionApiService;
+
+public class Injection {
+
+    public static ReunionRepository createReunionRepository() {
+        return new ReunionRepository(new DummyReunionApiService());
+    }
+
 }
