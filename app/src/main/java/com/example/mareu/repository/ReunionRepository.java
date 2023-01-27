fd91a2d7a5180db956ec37c5c019b199fe7a25b2
OLLAPION <ollapion@gmail.com>
2023-01-27 20:18:29 +0100

    Repository qui reprend le fakeApi

--- app/src/main/java/com/example/mareu/repository/ReunionRepository.java
+++ app/src/main/java/com/example/mareu/repository/ReunionRepository.java
@@ -1,2 +1,27 @@
-package com.example.mareu.repository;public class ReunionRepository {
+package com.example.mareu.repository;
+
+import com.example.mareu.model.Reunion;
+import com.example.mareu.service.ReunionApiService;
+
+import java.util.List;
+
+public class ReunionRepository {
+
+    private final ReunionApiService reunionApiService;
+
+    public ReunionRepository(ReunionApiService reunionApiService) {
+        this.reunionApiService = reunionApiService;
+    }
+
+    public List<Reunion> getReunion() {
+        return reunionApiService.getReunion();
+    }
+
+    public void addReunion(Reunion reunion) {
+        reunionApiService.addReunion(reunion);
+    }
+
+    public void deleteReunion(Reunion reunion) {
+        reunionApiService.deleteReunion(reunion);
+    }
 }
