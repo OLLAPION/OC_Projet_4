9a308afbae8023e528b4b8b816eadfb2f6be7417
OLLAPION <ollapion@gmail.com>
2023-01-27 23:26:33 +0100

    Modification du generator

--- app/src/main/java/com/example/mareu/service/DummyReunionGenerator.java
+++ app/src/main/java/com/example/mareu/service/DummyReunionGenerator.java
@@ -13,7 +13,10 @@ public abstract class DummyReunionGenerator {
     }
 
     public static List<Reunion> DUMMY_REUNION = Arrays.asList(
-            new Reunion(0, "Salle A", "24/01/2023", "10h30", "12h00", "Sujet 01", Arrays.asList("participant1@monadresse.fr", "participant2@monadresse.fr", "participant3@monadresse.fr"))
+            new Reunion(0, "Salle A", "24/02/2023", "10h30", "12h00", "Sujet 01", Arrays.asList("participant1@monadresse.fr", "participant2@monadresse.fr", "participant3@monadresse.fr")),
+            new Reunion(1, "Salle A", "24/02/2023", "9h30", "10h30", "Sujet 02", Arrays.asList("participant1@monadresse.fr", "participant2@monadresse.fr", "participant3@monadresse.fr")),
+            new Reunion(2, "Salle B", "25/02/2023", "14h00", "18h00", "Sujet 03", Arrays.asList("participant1@monadresse.fr", "participant2@monadresse.fr")),
+            new Reunion(3, "Salle C", "26/02/2023", "13h30", "15h00", "Sujet 04", Arrays.asList("participant1@monadresse.fr", "participant2@monadresse.fr", "participant3@monadresse.fr", "participant3@monadresse.fr"))
             );
 
 }
