fb032d9b1b1bf630edbb4c1d305d20826d81c828
OLLAPION <ollapion@gmail.com>
2023-01-28 06:46:57 +0100

    modification du DI injection pour l'Api et le repository

--- app/src/main/java/com/example/mareu/di/Injection.java
+++ app/src/main/java/com/example/mareu/di/Injection.java
@@ -2,11 +2,30 @@ package com.example.mareu.di;
 
 import com.example.mareu.repository.ReunionRepository;
 import com.example.mareu.service.DummyReunionApiService;
+import com.example.mareu.service.ReunionApiService;
 
+/**
+ * Injection de dependance pour aller chercher une instance des services
+ */
 public class Injection {
 
+    /**
+     * cherche toujours une nouvelle instance de @{@link ReunionRepository}. Utilisable pour les tests.
+     * @return
+     */
     public static ReunionRepository createReunionRepository() {
         return new ReunionRepository(new DummyReunionApiService());
     }
+
+    private static final ReunionApiService service = new DummyReunionApiService();
+
+    /**
+     * chercher une instance de  @{@link ReunionApiService}
+     * @return
+     */
+    public static ReunionApiService getReunionApiService() {
+        return service;
+    }
+
 
 }
